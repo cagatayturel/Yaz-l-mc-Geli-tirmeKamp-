52c2b835e865117f0e4cd63c33c8ad1cad117381
cagatayturel <cagatayturel@gmail.com>
2021-02-17 18:59:37 +0300

    Create Musteri.cs

---
+++ Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev3
+{
+    class Musteri
+    {
+        public int ID { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public int MusteriYasi { get; set; }
+
+
+    }
+}
