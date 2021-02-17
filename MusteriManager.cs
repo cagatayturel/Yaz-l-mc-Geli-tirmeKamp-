fcb0918e8534e02340a67a46301ede925b72330f
cagatayturel <cagatayturel@gmail.com>
2021-02-17 18:55:43 +0300

    BankDemo

---
+++ MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev3
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(List<Musteri> musterilistesi, Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklenmiştir" + musteri.MusteriAdi+ musteri.MusteriSoyadi);
+            musterilistesi.Add(musteri);
+
+        }
+
+        public void MusteriListele(List<Musteri> musterilistesi)
+        {
+            
+            foreach (var musteri in musterilistesi)
+            {
+                Console.WriteLine("Müşteriler:" + musteri.ID + musteri.MusteriAdi + musteri.MusteriSoyadi + musteri.MusteriYasi);
+            }
+        }
+        public void MusteriSil(List<Musteri> musterilistesi, Musteri musteri)
+        {
+            Console.WriteLine("Musteri silinmiştir" + musteri.MusteriAdi + musteri.MusteriSoyadi);
+            musterilistesi.Remove(musteri);
+        }
+    }
+    
+}
